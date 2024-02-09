dc02aa202c7fde1ff54757f3b20a831ab173cde5
Gary Pham <gpmastertaz@gmail.com>
2024-02-09 22:08:37 +1100

    update suspense in layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import Header from "./components/Header";
 import Providers from "./Providers";
 import Navbar from "./components/Navbar";
 import SearchBox from "./components/SearchBox";
-// import { Suspense } from 'react';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
